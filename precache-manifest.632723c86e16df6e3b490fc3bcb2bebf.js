356245a026a7a02a6c0837cff1566c6ad3d994d4
juanidiaz <jdi@idiaz.ca>
2019-04-06 22:57:09 -0400

    Updates

---
+++ precache-manifest.632723c86e16df6e3b490fc3bcb2bebf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "11473c398399abd2653d",
+    "url": "/clickandremember/static/js/runtime~main.5fef9043.js"
+  },
+  {
+    "revision": "f8280ef99a79aa113c97",
+    "url": "/clickandremember/static/js/main.797d9c75.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/clickandremember/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f8280ef99a79aa113c97",
+    "url": "/clickandremember/static/css/main.d37e9f0b.chunk.css"
+  },
+  {
+    "revision": "7468d5f9e90572706b604cd657ec7bed",
+    "url": "/clickandremember/index.html"
+  }
+];
